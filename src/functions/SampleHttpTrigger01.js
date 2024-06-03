d3eeccc3018596c71027aaae0f74a7574f783256
oskar charles <oskar.charles@gmail.com>
2024-06-03 11:13:01 -0600

    reading values from key vault

--- src/functions/SampleHttpTrigger01.js
+++ src/functions/SampleHttpTrigger01.js
@@ -1,4 +1,9 @@
 const { app } = require('@azure/functions');
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+require('dotenv').config();
+
+
 
 app.http('SampleHttpTrigger01', {
     methods: ['GET', 'POST'],
@@ -6,8 +11,47 @@ app.http('SampleHttpTrigger01', {
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
+        // 01. Get Config Keys
+        const sampleKey = process.env.sampleKey;
+        
+
+        // 02. Get Key Vault Secrets
+        const valueInKeyVault = await GetKeyVaultValues(sampleKey);
+
+        // 02. Save data on blob storage
         const name = request.query.get('name') || await request.text() || 'world';
 
-        return { body: `Hello, ${name}!` };
+        //return { body: `Hello, ${name}!` };
+
+        return { body: `sample value from keyvault, ${valueInKeyVault.value}!` };
     }
 });
+
+
+async function GetKeyVaultValues(secretName){
+
+    try {
+        // Authenticate to Azure
+        const credential = new DefaultAzureCredential(); 
+
+        // Create SecretClient
+        //const keyVaultName =  process.env.keyvaultName; 
+        const url = `https://${process.env.keyvaultName}.vault.azure.net`;  
+        const client = new SecretClient(url, credential); 
+        var secret = '';
+
+        // Get secret
+        try {
+            secret = await client.getSecret(secretName);
+            console.log(secret.value);
+        } catch (error) {
+            console.log(error);
+            
+        }
+        
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+}
